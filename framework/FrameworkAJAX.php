e41e412af137d1fa752e5e6f1678e3b49a1633eb
skelpo <info@skelpo.com>
2012-12-24 12:23:36 +0100

    Update framework/FrameworkAJAX.php

--- framework/FrameworkAJAX.php
+++ framework/FrameworkAJAX.php
@@ -4,11 +4,27 @@
  * 
  * @author Ralph Küpper, Serhiy Medvedyev
  * 
- * This script is part of the gwwFramework.
  * 
+ *  This script is part of the gwwFramework. Developed and produced by Skelpo - hot software.
+ *  If you have any questions, recommendations or general requests visit our website
+ *  http://www.skelpo.com or write an email to info@skelpo.com
+ * 
+ *  Copyright (C) 2012  Skelpo
  *
- */
-
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * */
 
 /*
  * Needed infos from the calling index.php
@@ -155,3 +171,4 @@ ob_end_flush();
 
 
 
+?>
